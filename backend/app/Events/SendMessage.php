da28304beb86ac0bb5690e3eb1b4e4f4f1d7caab
axell-brendow <breno.axel@gmail.com>
2020-07-19 01:33:54 -0300

    Create SendMessage event to broadcast it

---
+++ backend/app/Events/SendMessage.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Events;
+
+use App\Message;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class SendMessage implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * @var \App\Message
+     */
+    private $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('received-messages');
+    }
+
+    public function broadcastWith()
+    {
+        return $this->message->toArray();
+    }
+}
