51b1330826e7768fdc630e450437b10e24d9e384
axell-brendow <breno.axel@gmail.com>
2020-07-19 11:46:54 -0300

    Add dockerize tool
    When the Laravel API container is going up, I use dockerize to
    wait the database to be ready before I try to do the migrations

--- backend/entrypointAPI.sh
+++ backend/entrypointAPI.sh
@@ -8,17 +8,9 @@ composer install
 
 # LARAVEL
 
-function tryToMigrate() {
-    timeout=5
-    php artisan migrate:fresh --seed
+# Try to connect to the database every 1 second over 40 seconds
+dockerize -wait tcp://db:5432 -timeout 40s
 
-    if [ $? -ne 0 ]; then
-        echo "Database not ready. I will try to reconnect in $timeout seconds..."
-        sleep $timeout
-        tryToMigrate
-    fi
-}
-
-tryToMigrate
+php artisan migrate:fresh --seed
 
 php artisan serve --host 0.0.0.0 --port 8000
